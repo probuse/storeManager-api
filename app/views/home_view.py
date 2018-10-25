b9d513db8d3f2186b6f2cf0d7d5343d79b4c3746
probuse <nprobuse@gmail.com>
2018-10-26 00:22:41 +0300

    fix failing code

--- app/views/home_view.py
+++ app/views/home_view.py
@@ -9,7 +9,7 @@ class HomeEndPoint(Resource):
     def get(self):
         'Returns home view'
         return {
-            "message": "Welcome to Store Manager API"
+            "message": "Welcome to Store Manager API",
             "active endpoints": {
                 'products' : "https://storemanager-p-api.herokuapp.com/api/v1/products",
                 'sales': "https://storemanager-p-api.herokuapp.com/api/v1/sales"

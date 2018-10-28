b0f7fe3accde263dd72c894933770ed140f810c3
probuse <nprobuse@gmail.com>
2018-10-28 09:17:28 +0300

    rename tests/test_sales_endpoint to tests/test_sales_views

---
+++ tests/test_sales_views.py
@@ -0,0 +1,88 @@
+"""
+    This module will contain tests for the sales endpoint
+"""
+from tests.base_tests import BaseTestCase
+from app.models.sale import Sale
+
+class SaleTestCase(BaseTestCase):
+    "TestCase to test SaleEndPoint"
+
+    def test_add_sale_returns_200_status_code(self):
+        "Test adding sale returns 201 status code"
+        with self.client:
+            response = self.add_sale(1, 5, 1)
+            self.assertEqual(response.status_code, 200)
+
+    def test_add_sale_returns_message_to_user(self):
+        "Test adding sale returns information of added sale"
+        with self.client:
+            self.add_product("egg", 500)
+            sale_response = self.add_sale(1, 2, 1)
+            self.assertIn(b'2 egg(s) successfully sold', sale_response.data)
+
+    def test_add_sale_if_product_id_doesnot_exist(self):
+        "Test adding sale with invalid product_id"
+        with self.client:
+            self.add_product("egg", 500)
+            sale_response = self.add_sale(3, 2, 1)
+            self.assertIn(b'Product with Product id 3 does not exist', sale_response.data)
+
+    def test_get_sales_returns_200_status_code(self):
+        "Test a GET request to /sales returns a 200 status code"
+        with self.client:
+            response = self.get_sales()
+            self.assertEqual(response.status_code, 200)
+
+    def test_get_sales_when_no_sales_have_been_made(self):
+        "Test request to /sales returns a message to user with no sales"
+        with self.client:
+            response = self.get_sales()
+            self.assertIn(b'No Sales made yet', response.data)
+
+    def test_get_sales_when_sales_have_been_made(self):
+        "Test request to /sales returns a message to user with sales"
+        with self.client:
+            self.add_product("egg", 500)
+            self.add_sale(1, 2, 1)
+            response = self.get_sales()
+            self.assertIn(b'2', response.data)
+
+    def test_get_a_sale_returns_200_status_code(self):
+        "Test /sales/<sale_id> endpoint responds with a 200 status code"
+        with self.client:
+            self.add_product("egg", 500)
+            self.add_sale(1, 2, 1)
+            response = self.get_a_sale(1)
+            self.assertEqual(response.status_code, 200)
+
+    def test_get_a_sale_returns_no_products_yet(self):
+        "Tests if get_a_sale returns with no product added"
+        with self.client:
+            response = self.get_a_sale(1)
+            self.assertIn(b'{"message": "No products added yet"}', response.data)
+
+    def test_get_a_sale_returns_no_sales_yet(self):
+        "Tests if get_a_sale returns with no sales added"
+        with self.client:
+            self.add_product("egg", 500)
+            response = self.get_a_sale(1)
+            self.assertIn(b'{"message": "No Sales made yet"}', response.data)
+
+    def test_get_a_sale_returns_sale(self):
+        "Tests if get_a_sale returns when a sale is added"
+        with self.client:
+            self.add_product("egg", 500)
+            response = self.add_sale(1, 2, 1)
+            self.assertIn(b'{"message": "2 egg(s) successfully sold"}', response.data)
+
+    def test_get_a_sale_returns_sale_does_not_exist(self):
+        "Tests if get_a_sale returns when a sale is added"
+        with self.client:
+            self.add_product("egg", 500)
+            response = self.add_sale(3, 2, 1)
+            self.assertIn(
+                b'{"message": "Product with Product id 3 does not exist"}', 
+                response.data
+            )
+
+    

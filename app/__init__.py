cc1da7aa5afa251b9c1a551764069d2bc9602065
probuse <nprobuse@gmail.com>
2018-11-01 10:33:52 +0300

    add logic to switch configuration classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 """
     This module will contain the application
 """
+import os
 from flask import Flask
 from config import DevelopmentConfig, TestingConfig
 
 app = Flask(__name__)
-config_class = ""
-
-app.config.from_object(DevelopmentConfig)
-print(app.config['DATABASE_URL'])
+config_class = None
+if os.getenv('CONFIG_CLASS') == "development":
+    config_class = DevelopmentConfig
+elif os.getenv('CONFIG_CLASS') == "testing":
+    config_class = TestingConfig
+app.config.from_object(config_class)
 from app.routes import ProductEndPoint, SingleProductEndPoint, SaleEndPoint, SingleSaleEndPoint 

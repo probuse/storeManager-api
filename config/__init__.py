03f7ebe787133bbbc6ab0d4551ab3b543697d8f3
probuse <nprobuse@gmail.com>
2018-11-02 09:53:16 +0300

    add heroku postgres db

--- config/__init__.py
+++ config/__init__.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/storemanager_np'
+    DATABASE_URL = 'postgres://bijzbcquzvtjyv:cfa4a3d9a8436ac6a0059cf1f776e24b5bd560f881ebe01237fbb8a6ea318646@ec2-54-204-14-96.compute-1.amazonaws.com:5432/dnm38gidg41rr'
 
 
 class DevelopmentConfig(Config):
